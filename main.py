9bf9806be157ce4b62d373ac9e9a06d6fc7fe2db
Siarhei Kushniaruk <siarhei_kushniaruk@epam.com>
2025-08-26 00:28:28 +0200

    go

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
+import torch
+
+
+
 # Analyze dataset for vocab_size
-from email import message
-
-
 with open('dataset.txt', 'r', encoding='utf-8') as file:
     text = file.read()
 
 chars = sorted(list(set(text)))
 vocab_size = len(chars)
 print(f'Total chars in dataset is: {vocab_size}')
+
 
 
 # Implement encoder and decoder
@@ -30,3 +32,53 @@ def decode(message: list[int]) -> str:
     return result
 
 print(f'{decode(encode('Лев Толстой'))} <--> {encode('Лев Толстой')}')
+print('---')
+
+
+
+# Encode the dataset and store it into torch.tensor
+data = torch.tensor(encode(text), dtype=torch.long)
+
+
+
+# Split dataset into train (90%) and validation (10%) sets
+n = int(0.9*len(data))
+train_data = data[:n]
+val_data = data[n:]
+
+
+
+# Split dataset on chunks
+torch.manual_seed(1337)
+block_size = 8                  # how many context characters we gonna use for prediction
+batch_size = 4                  # how many independent sequences we gonna process in parallel
+
+def get_batch(split: str) -> (list[int], list[int]):
+    if split == 'train':
+        data = train_data
+    else:
+        data = val_data
+
+    ix = torch.randint(len(data) - block_size, (batch_size,))   # create 4 (batch_size) tensors of 8 (block_size) characters length on random positions in data
+    x_list = []
+    y_list = []
+    for i in ix:
+        x_list.append(data[i:i+block_size])
+        y_list.append(data[i+1:i+block_size+1])
+    return torch.stack(x_list), torch.stack(y_list)
+
+
+xb, yb = get_batch('train')
+print(f'Inputs: {xb.shape}')
+print(xb)
+print(f'Targets: {yb.shape}')
+print(yb)
+
+print('---')
+
+for batch in range(batch_size):
+    print(f'Batch {batch}')
+    for ch in range(block_size):
+        context = xb[batch, :ch+1]
+        target = yb[batch, ch]
+        print(f'When input is {context.tolist()} the target is {target}')
