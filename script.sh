bf743e7d4892d9b681f1eadc2c2adbe4a398c2d0
Siarhei Kushniaruk <siarhei_kushniaruk@epam.com>
2025-09-01 22:08:28 +0200

    Add 2nd GitHub MCP

--- script.sh
+++ script.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
 
-pip install uv
-uv --version
+pip3 venv .venv
+source .venv/bin/activate
 
-uv venv --python 3.13 --path .venv
+pip3 install -r requirements.txt
 
-# Windows Git Bash compatible activation
-if [ -f ".venv/Scripts/activate" ]; then
-    source .venv/Scripts/activate
-elif [ -f ".venv/bin/activate" ]; then
-    source .venv/bin/activate
-else
-    echo "Virtual environment activation script not found"
-    exit 1
-fi
-
-uv add -r requirements.txt
-
-uv run main.py
+python3 main.py
