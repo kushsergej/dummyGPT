75c9066e2c8f41fac3801136a82c4237f6654bee
Siarhei Kushniaruk <siarhei_kushniaruk@epam.com>
2025-09-01 23:03:38 +0200

    Add 2nd GitHub MCP

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 python -m pip install --upgrade uv
-uv venv .venv --python 3.12
+uv venv .venv --python 3.12 --clear
 source .venv/Scripts/activate
 
 uv pip install -r requirements.txt
